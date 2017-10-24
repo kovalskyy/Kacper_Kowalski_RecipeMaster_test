76e9d1a29c36bb6e13c2c7ea23fa3c4aff442250
Kacper Kowalski <kowal@Mac-mini-Kacper.local>
2017-10-24 18:09:14 +0200

    created basic UI setup with controllers

---
+++ RecipeMaster_recruitment/DetailsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DetailsViewController.swift
+//  RecipeMaster_recruitment
+//
+//  Created by Kacper Kowalski on 24.10.2017.
+//  Copyright © 2017 Kacper Kowalski. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.automaticallyAdjustsScrollViewInsets = false
+    }
+
+    
+
+}
---
+++ RecipeMaster_recruitment/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  RecipeMaster_recruitment
+//
+//  Created by Kacper Kowalski on 24.10.2017.
+//  Copyright © 2017 Kacper Kowalski. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    
+
+}
+
