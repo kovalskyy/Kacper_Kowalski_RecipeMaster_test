0f737ad35a4f2d95502c6ebe6ddd84609c5ebe02
Kacper Kowalski <kowal@Mac-mini-Kacper.local>
2017-10-25 16:58:35 +0200

    simulator working on iOS9.0

--- RecipeMaster_recruitment/AppDelegate.swift
+++ RecipeMaster_recruitment/AppDelegate.swift
@@ -22,7 +22,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
-        return FBSDKApplicationDelegate.sharedInstance().application(app, open: url, options: options)
+        
+        let handled = FBSDKApplicationDelegate.sharedInstance().application(app, open: url, sourceApplication: options[UIApplicationOpenURLOptionsKey.sourceApplication] as! String, annotation: options[UIApplicationOpenURLOptionsKey.annotation])
+        return handled
     }
     
     func applicationWillResignActive(_ application: UIApplication) {
